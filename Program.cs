3af9d535c0ded8cfa51f9896390d763c1c2fc747
ukiiikkkkkk <azry_syauqi_34pg@student.smktelkom-mlg.sch.id>
2026-03-12 10:20:00 +0700

    MembuatClassUser

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
 ﻿
+public class user
+{
+    public string nama;
+    public string email;
+    public string password;
+    public string role;
+
+    public user(string nama, string email, string password, string role)
+    {
+        this.nama = nama;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public void TampilkanInfo()
+    {
+        Console.WriteLine($"Nama: {nama} | Role: {role}");
+    }
+}
