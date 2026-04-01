2a40b3356029cb42eb035f649b0bc2e5ae1aac8b
ukiiikkkkkk <azry_syauqi_34pg@student.smktelkom-mlg.sch.id>
2026-04-01 09:18:39 +0700

    membuat user induk dan turunan

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
-﻿
+﻿using System.Reflection.Emit;
+Siswa siswaObj = new Siswa("Kolip", "Kolip@gmail.com", "Kolip123", "Siswa", "X DKV 1");
+Guru guruObj = new Guru("Pak Budi", "Budi01@gmail,com", "Budi123", "Guru", "Matematika");
+
+siswaObj.TampilkanInfo();
+siswaObj.SubmitTugas();
+
+Console.WriteLine();
+
+guruObj.TampilkanInfo();
+guruObj.NilaiTugas();
+
 public class user
 {
     public string nama;
@@ -16,6 +27,37 @@ public class user
 
     public void TampilkanInfo()
     {
-        Console.WriteLine($"Nama: {nama} | Role: {role}");
+        Console.WriteLine($"Nama: {nama} |  Role : {role}");
+    }
+}
+class Siswa : user
+{
+
+    public string className;
+
+    public Siswa(string nama, string email, string password, string role, string classname) : base(nama, email, password, role)
+    {
+        classname = classname;
+    }
+
+    public void SubmitTugas(string nama, string className)
+    {
+        Console.WriteLine($"{nama} dari kelas {className} sudah mengumpulkan tugas");
+    }
+
+}
+
+class Guru : user
+{
+    public string mapel;
+
+    public Guru(string nama, string email, string password, string role, string mapel) : base(nama, email, password, role)
+    {
+        mapel = mapel;
+    }
+
+    public void NilaiTugas()
+    {
+        Console.WriteLine($"Guru {nama} telah manilai semua tugas {mapel}");
     }
 }
